1af3965a9889e9b68ae5d7ab8d3b82db20948b95
naveenblackdevil <144346339+naveenblackdevil@users.noreply.github.com>
2023-09-08 17:24:24 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+def fact_rec(n):
+  if n == 0 or n==1:
+   return 1
+  else:
+  I return n * faxt_rec(n - 1)
+
+
+ number = int(input("Enter a value: ")
+res = fact_rec( number )
+
+print("the factorial of {} is {}.". forma( number ,res))
+              
+              
+              
+   
+  
